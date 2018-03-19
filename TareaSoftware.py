01d5a8a3c0d95fc11748c06788f0295a85b2b530
Blacky <juancontre91@gmail.com>
2018-03-19 01:43:43 -0300

    Mejorando los detalles antes de generar la matriz

--- TareaSoftware.py
+++ TareaSoftware.py
@@ -18,24 +18,24 @@ def regresiva(M,N,H):
 	return U1,U2,U3
 
 
-intervalos=int(input("Ingrese cantidad de nodos internos"))
+intervalos=int(input("Ingrese cantidad de nodos internos: "))
 h = float(1/intervalos) 
-x_ini=int(input("Ingrese valor de x(0)"))
-x_fin=int(input("Ingrese valor de x(1)"))
+x_ini=int(input("Ingrese valor de x(0): "))
+x_fin=int(input("Ingrese valor de x(1): "))
 M=int(input("Ingrese M"))
 N=int(input("Ingrese N))
 C=int(input("Ingrese C"))
-F=input("ingrese la funionF(x)")
-num_deri=int(input("Ingrese el tipo de derivada a usar"))
+F=input("ingrese la funionF(x): ")
+num_deri=int(input("Ingrese el tipo de aproximacion va a usar: "))
 print("Centrada=1")
 print("Progresiva=2")
 print("Regresiva=3")
 if num_deri ==1 :
-	print(centrada(x,h))
+	print(centrada(M,N,h))
 elif num_deri ==2:
-	print(progresiva(x,h))
+	print(progresiva(M,N,h))
 elif num_deri ==3:
-	print(regresiva(x,h))
+	print(regresiva(M,N,h))
 
 
 
