7828e3745bfdf9585314fce22b5f86331c949387
Blacky <juancontre91@gmail.com>
2018-03-19 01:30:51 -0300

    Avanzando de apoco

--- TareaSoftware.py
+++ TareaSoftware.py
@@ -1,38 +1,41 @@
 import math 
+def centrada(M,N,H):
+	U1 = ((M/(h**2))+(N/2*h))
+	U2 = (-2*M/(h**2))
+	U3 = ((M/(h**2))-(N/2*h))
+	return U1,U2,U3
 
-def funcion(x):
-	resultado=pow(x,3)+pow(x,2)+x+3
-	return resultado
+def progresiva(M,N,H):
+        U1 = (M/(h**2))
+	U2 = (((-2*M)/(h**2))-(1/h))
+	U3 = ((M/(h**2))+(N/h))
+	return U1,U2,U3
 
-def centrada(x):
-	deri=(funcion(x+h)-funcion(x-h))/(2*h)
-	return deri
+def regresiva(M,N,H):
+	U1 = (M/(h**2)-(N/h))
+	U2 = (((-2*M)/(h**2))+(N/h))
+        U3 = (M/(h**2))
+	return U1,U2,U3
 
-def progresiva(x):
-	deri=(funcion(x+h)+funcion(x))/h
-	return deri
 
-def regresiva(x):
-	deri=(funcion(x)-funcion(x-h))/h
-	return deri
-def segundaDerivada (x):
-	deri=((funcion(x-h)-2*funcion(x)+funcion(x-h))/h**2)
-	return deri
-
-x=0
-xfin=1
 intervalos=int(input("Ingrese cantidad de nodos internos"))
 h = float(1/intervalos) 
+x_ini=int(input("Ingrese valor de x(0)"))
+x_fin=int(input("Ingrese valor de x(1)"))
+M=int(input("Ingrese M"))
+N=int(input("Ingrese N))
+C=int(input("Ingrese C"))
+F=input("ingrese la funionF(x)")
 num_deri=int(input("Ingrese el tipo de derivada a usar"))
 print("Centrada=1")
 print("Progresiva=2")
 print("Regresiva=3")
 if num_deri ==1 :
-	print(centrada(x))
+	print(centrada(x,h))
 elif num_deri ==2:
-	print(progresiva(x))
+	print(progresiva(x,h))
 elif num_deri ==3:
-	print(regresiva(x))
+	print(regresiva(x,h))
 
 
 
