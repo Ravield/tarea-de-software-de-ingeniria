94baa1e505524f9253a0e8bc490e34a96dec4cf1
Blacky <juancontre91@gmail.com>
2018-03-19 10:52:41 -0300

    Casi lista matriz B, y algunos cambios

--- TareaSoftware.py
+++ TareaSoftware.py
@@ -1,43 +1,66 @@
 import math 
-def centrada(M,N,H):
-	U1 = ((M/(h**2))+(N/2*h))
-	U2 = (-2*M/(h**2))
-	U3 = ((M/(h**2))-(N/2*h))
-	return U1,U2,U3
-
-def progresiva(M,N,H):
-        U1 = (M/(h**2))
-	U2 = (((-2*M)/(h**2))-(1/h))
-	U3 = ((M/(h**2))+(N/h))
-	return U1,U2,U3
-
-def regresiva(M,N,H):
-	U1 = (M/(h**2)-(N/h))
-	U2 = (((-2*M)/(h**2))+(N/h))
-        U3 = (M/(h**2))
-	return U1,U2,U3
-
-
 intervalos=int(input("Ingrese cantidad de nodos internos: "))
-h = float(1/intervalos) 
+h = float(1/(intervalos+1)) 
+print(h)
+#Creo lista A de largo (intervalos-1)
+A = [0] * (intervalos)
+#Cada elemento de la lista A será de largo (intervalos-1) [Matriz cuadrada]
+for i in range(intervalos):
+    A[i] = [0] * (intervalos)
+
+#Creo lista B [Matriz de una columna]
+B = [0] * (intervalos-1)
+
 x_ini=int(input("Ingrese valor de x(0): "))
 x_fin=int(input("Ingrese valor de x(1): "))
-M=int(input("Ingrese M"))
-N=int(input("Ingrese N))
-C=int(input("Ingrese C"))
-F=input("ingrese la funionF(x): ")
-num_deri=int(input("Ingrese el tipo de aproximacion va a usar: "))
+M=int(input("Ingrese M: "))
+N=int(input("Ingrese N: "))
+C=int(input("Ingrese C: "))
+F=math.sin(math.pi/2)
+print(F)
+print("Aproximaciones")
 print("Centrada=1")
 print("Progresiva=2")
 print("Regresiva=3")
-if num_deri ==1 :
-	print(centrada(M,N,h))
-elif num_deri ==2:
-	print(progresiva(M,N,h))
-elif num_deri ==3:
-	print(regresiva(M,N,h))
+num_apx=int(input("Ingrese el tipo de aproximacion que va a usar: "))
+if num_apx ==1 :
+        U1 = ((M)+(h*N/2))
+        U2 = (-2*M)
+        U3 = ((M)-(h*N/2))
+        
+elif num_apx ==2:
+        U1 = (M)
+        U2 = ((-2*M)-(N*h))
+        U3 = ((M)+(h*N))
+        
+elif num_apx ==3:
+        U1 = (M-(N*h))
+        U2 = ((-2*M)+(N*h))
+        U3 = (M)
+        
+	
+for i in range(intervalos):
+        for j in range(intervalos):
+                if i==j :
+                        A[i][j]= U2
+                elif j == i+1:
+                        A[i][j]= U3
+                elif j == i-1:
+                        A[i][j]= U1
+                else:
+                        A[i][j]= 0
+print("la matriz A es la siguiente:")
+print('\n'.join([''.join(['{:10}'.format(item) for item in row]) 
+      for row in A]))
 
-
-
-
-
+for i in range(intervalos):
+        print(i)
+        if i == 0:
+                B[i] = h**2*(F-C)-(x_ini)
+                print(B)
+        elif i == intervalos-1:
+                B[i] =  h**2*(F-C)-(x_fin)
+        else:
+                B[i] =  h**2*(F-C)
+print("la metriz B es:")
+print(B)
